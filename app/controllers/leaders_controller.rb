a24430bc1ceadf3e2d5708e9e40def2271c6733b
Alex Gorski <alex.gorski@gmail.com>
2013-04-23 11:21:59 -0400

    refactored leaders controller and moved logic to users model

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,9 +1,9 @@
 class LeadersController < ApplicationController
   def index
-    @level1 = User.where(:level=>1).all.sort_by! {|u| u[:right]}.reverse
-    @level2 = User.where(:level=>2).all.sort_by! {|u| u[:right]}.reverse
-    @level3 = User.where(:level=>3).all.sort_by! {|u| u[:right]}.reverse
-    @level4 = User.where(:level=>4).all.sort_by! {|u| u[:right]}.reverse
-    @level5 = User.where(:level=>5).all.sort_by! {|u| u[:right]}.reverse
+    @level1 = User.make_leaders(1)
+    @level2 = User.make_leaders(2)
+    @level3 = User.make_leaders(3)
+    @level4 = User.make_leaders(4)
+    @level5 = User.make_leaders(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :operator, :level, :right, :wrong, :score
+
+  def self.make_leaders(level)
+    leaders = User.where(:level=>level).all.sort_by! {|u| u[:right]}.reverse
+  end
 end
