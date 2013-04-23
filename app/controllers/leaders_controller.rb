bbaeb518adc217d214967aa8b2c8af7f4bd7cab0
Alex Gorski <alex.gorski@gmail.com>
2013-04-23 11:13:02 -0400

    fixed sort_b

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,9 +1,9 @@
 class LeadersController < ApplicationController
   def index
-    @level1 = User.where(:level=>1).all.sort_by! {|u,v| u.right <=> v.right}.reverse
-    @level2 = User.where(:level=>2).all.sort_by! {|u,v| u.right <=> v.right}.reverse
-    @level3 = User.where(:level=>3).all.sort_by! {|u,v| u.right <=> v.right}.reverse
-    @level4 = User.where(:level=>4).all.sort_by! {|u,v| u.right <=> v.right}.reverse
-    @level5 = User.where(:level=>5).all.sort_by! {|u,v| u.right <=> v.right}.reverse
+    @level1 = User.where(:level=>1).all.sort_by! {|u| u[:right]}.reverse
+    @level2 = User.where(:level=>2).all.sort_by! {|u| u[:right]}.reverse
+    @level3 = User.where(:level=>3).all.sort_by! {|u| u[:right]}.reverse
+    @level4 = User.where(:level=>4).all.sort_by! {|u| u[:right]}.reverse
+    @level5 = User.where(:level=>5).all.sort_by! {|u| u[:right]}.reverse
   end
 end
